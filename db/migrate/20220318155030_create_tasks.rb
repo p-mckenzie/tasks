f253daf5c5d67c2fb5891820e89c0a1c104e6044
Paige McKenzie <paige.a.mckenzie@gmail.com>
2022-04-04 18:51:08 -0400

    Seed database

--- db/migrate/20220318155030_create_tasks.rb
+++ db/migrate/20220318155030_create_tasks.rb
@@ -5,7 +5,6 @@ class CreateTasks < ActiveRecord::Migration[6.1]
       t.text :description
       t.date :due_date, null: false
 
-      t.boolean :is_recurring, default: false
       t.string :recurrence_type, default: ""
       t.integer :separation
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Group.create(title: "Test organization", description: "Holds seed tasks and users")
+
+User.create(first_name: 'Paige', last_name: 'McKenzie', email: 'mp@test.com', password: 'testtest', password_confirmation: 'testtest')
+UserGroupAssignment.create(user:User.first, group:Group.first, admin: true)
+
+User.create(first_name: 'John', last_name: 'Doe', email: 'jd@test.com', password: 'testtest', password_confirmation: 'testtest')
+UserGroupAssignment.create(user:User.last, group:Group.first)
+
+Group.first.tasks << Task.new(title: "Weekly recurring item", description: '', due_date: Date.today + 1, recurrence_type: "Weekly", separation: 1)
+Group.first.tasks << Task.new(title: "Daily recurring item", description: '', due_date: Date.today + 1, recurrence_type: "Daily", separation: 1)
+Group.first.tasks << Task.new(title: "Single instance item", description: '', due_date: Date.today + 1)
